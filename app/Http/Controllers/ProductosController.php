8e5e4b2ddb62cec6f37c16cdcb50cfd11dc1417a
Juan Ramón <juanriveiro@gmail.com>
2021-06-11 09:32:45 +0000

    feat: firest commit
    Laravel empty with a basic controller

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductosController extends Controller
+{
+	// Esto es un comentario
+	public function index() {
+		return dd("lalala");
+    }
+}
